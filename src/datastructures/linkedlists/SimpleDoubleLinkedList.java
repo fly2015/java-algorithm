8f3f5e10d3104bcf30819251c02c113b2a6dc9ad
Hien Ng <hien.nguyenhuuquoc@axonactive.com>
2023-07-12 18:38:49 +0700

    Change information

--- src/datastructures/linkedlists/SimpleDoubleLinkedList.java
+++ src/datastructures/linkedlists/SimpleDoubleLinkedList.java
@@ -1,9 +1,9 @@
 /*
  * SimpleDoubleLinkedList.java
  *
- * Copyright by CRIF AG
- * Zürich
- * All rights reserved.
+ * Copyright by Hien Ng
+ * Da Nang
+
  */
 package datastructures.linkedlists;
 
--- src/datastructures/linkedlists/SimpleSingleLinkedList.java
+++ src/datastructures/linkedlists/SimpleSingleLinkedList.java
@@ -1,9 +1,9 @@
 /*
  * SimpleSingleLinkedList.java
  *
- * Copyright by CRIF AG
- * Zürich
- * All rights reserved.
+ * Copyright by Hien Ng
+ * Da Nang
+
  */
 package datastructures.linkedlists;
 
